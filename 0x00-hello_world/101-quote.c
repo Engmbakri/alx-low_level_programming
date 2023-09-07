1c720475a5b4928fa0eccd104400bb5bcf622de6
Emgmbakri <mrm507415@gmail.com>
2023-09-07 09:48:49 -0700

    fuck c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
  *
- * Sescription: C program that prints with quo
+ * Description: C program that prints with quo function
+ * ssize_t write(int fd. const void *buf. size_t count); 
  *
  * Return: 1 (not success)
 */
