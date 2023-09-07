abe57a78ee19424cd7d7a4d010bd3dc59b0ca7d2
Emgmbakri <mrm507415@gmail.com>
2023-09-07 07:11:35 -0700

    by

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<tdio.h>
+#include<stdio.h>
 
 /**
  * main - entry point
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-printf"with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
 
