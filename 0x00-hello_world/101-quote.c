e9ca681509f2b670d915d1f6057b862505bc1b6b
Emgmbakri <mrm507415@gmail.com>
2023-09-07 09:53:31 -0700

    finaly

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  * main - Entry point
  *
  * Description: C program that prints with quo function
- * ssize_t write(int fd. const void *buf. size_t count); 
+ *
+ * ssize_t write(int fd. const void *buf. size_t count);
  *
  * Return: 1 (not success)
 */
