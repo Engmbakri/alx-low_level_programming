cd3ed275875cab401005b305fc39145b3b0d0dcf
Emgmbakri <mrm507415@gmail.com>
2023-09-07 06:20:43 -0700

    akheran

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Description: A C program that prints wit put function
  *
- * Return: always 0 (sucsses) 
+ * Return:Always 0 (sucsses)
 */
 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}	
+}
