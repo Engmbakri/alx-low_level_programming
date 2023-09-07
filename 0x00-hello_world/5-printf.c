1fbc4ecc639852ca230245a1ccc361f31aad15aa
Emgmbakri <mrm507415@gmail.com>
2023-09-07 07:20:53 -0700

    tbn

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
  * Description: C program thant prints with printf function
  *
- * retutn: Always 0 (success)
+ * retutn: Always 0 (Success)
 */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
