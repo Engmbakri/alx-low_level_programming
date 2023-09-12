3f50e3158e387b99cb37d5f0cfb5a5d2d0c35695
Emgmbakri <mrm507415@gmail.com>
2023-09-12 16:25:31 -0700

    k

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ for (min = 0 ; min <= 59 ; min++)
 _putchar((hr / 10) + 48);
 _putchar((hr % 10) + 48);
 _putchar(':');
--putchar((min / 10) + 48);
+_putchar((min / 10) + 48);
 _putchar((hr % 10) + 48);
 _putchar('\n');
 }
