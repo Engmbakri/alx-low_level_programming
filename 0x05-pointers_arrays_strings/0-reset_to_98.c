e483f07756c1a27ea94dbe9c52fb68d11c6f22a3
Emgmbakri <mrm507415@gmail.com>
2023-09-18 09:51:38 -0700

    kkk

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * rest_to_98 -takes a pointer to an int parameter
+ * reset_to_98 -takes a pointer to an int parameter
  *             and updates the value it points to 98
- * @c: int parameter
+ * @n: int parameter
  *
  * Return: nothing
 */
