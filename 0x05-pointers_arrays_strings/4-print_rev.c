64f4046965067dcea714f7c672cfd381e4b2b79d
Emgmbakri <mrm507415@gmail.com>
2023-09-18 12:41:26 -0700

    j

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - print reversed string, followed by new line
+ * @s: pointer to  string to print
+ *
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	whlie(s[i])
+		i++
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
