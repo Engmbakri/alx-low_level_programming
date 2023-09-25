d12c81837e8bffc77022bbf7dbac4f1af753ef54
Emgmbakri <mrm507415@gmail.com>
2023-09-25 11:19:08 -0700

    FB

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,11 @@ include "main.h"
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int dol;
 
-	for (i = 0 ; i < n ; i++)
+	for (dol = 0 ; dol < n ; dol++)
 	{
-		dest[i] = src[i];
+		dest[dol] = src[dol];
 	}
 
 	return (dest);
