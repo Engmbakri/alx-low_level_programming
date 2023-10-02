f2733b1d8722e31d25d71124b1d05b95f17c9ab2
Emgmbakri <mrm507415@gmail.com>
2023-10-02 09:41:54 -0700

    gh

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: nu-success, non-zero-fail
+ * Return: 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+	printf("%d\n", argc - 1);
+}
