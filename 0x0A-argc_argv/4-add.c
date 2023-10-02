32fe1317418ea4b85aa6fe0b2fd2a9dd937f62db
Emgmbakri <mrm507415@gmail.com>
2023-10-02 13:54:21 -0700

    g

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include < stdio.h>
+#include < stdlib.h>
+
+/**
+ * main - the entry point of the program
+ * @argc: the number of command-line
+ * @argv: array of strings containing the command-line
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc] ; *c ; c++)
+			if (*c < '0' || *C > 9)
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
