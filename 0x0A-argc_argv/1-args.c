34904d46cdd41b05e150214105dd26915d095222
Emgmbakri <mrm507415@gmail.com>
2023-10-02 09:51:07 -0700

    fg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,9 +2,9 @@
 
 /**
  * main - prints the number of arguments passed into it
- * @argc: number of command l command line argument
- * @argv: po-success, non-zer0-fail
- * Return: 0
+ * @argc: number of command line
+ * @argv: pointer to array of command line argumemt
+ * Return: 0-success, non-zero-fail.
 */
 
 int main(int argc, char *argv[] __attribute__((unused)))
