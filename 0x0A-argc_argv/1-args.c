740a88b05695e7eb1b5fc0a8e51bde1a811b9d37
Emgmbakri <mrm507415@gmail.com>
2023-10-02 09:46:50 -0700

    gh

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,10 +2,12 @@
 
 /**
  * main - prints the number of arguments passed into it
- * @argc: nu-success, non-zero-fail
+ * @argc: number of command l command line argument
+ * @argv: po-success, non-zer0-fail
  * Return: 0
 */
 
 int main(int argc, char *argv[] __attribute__((unused)))
+{
 	printf("%d\n", argc - 1);
 }
