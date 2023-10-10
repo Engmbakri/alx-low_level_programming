3fa1fe1efdd4a1343539dfd450e9bf7023a7a49c
Emgmbakri <mrm507415@gmail.com>
2023-10-10 05:18:24 -0700

    hH

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - print name of source file
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	printf("%s/n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
