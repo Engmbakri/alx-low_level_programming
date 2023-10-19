91e2c8366826278dc0baf72fe8741d111fc38e75
Emgmbakri <mrm507415@gmail.com>
2023-10-19 11:52:47 -0700

    gg

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,12 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t counter = 0;
+	size_t cont = 0;
 
 	while (h != NULL)
 	{
-		count++;
+		cont++;
 		h = h->next;
 	}
-	return (counter);
+	return (cont);
 }
