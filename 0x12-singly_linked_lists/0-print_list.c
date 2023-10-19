5a03e01ee6408cad8335572246df59ec69421e81
Emgmbakri <mrm507415@gmail.com>
2023-10-19 07:40:53 -0700

    gg

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,13 +17,13 @@ int _strlen(char *s)
 	return (i);
 }
 
-/***
+/**
  * print_list - function that prints all the elements of a list_t list
  * @h: pointer to first node
  * Return: the number of nodes
 */
 
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
