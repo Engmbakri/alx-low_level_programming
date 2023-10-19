68ec2839a36658ad4f4852980a2b0ba33b90e9a4
Emgmbakri <mrm507415@gmail.com>
2023-10-19 08:00:02 -0700

    hh

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -27,14 +27,11 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str != NULL)
-			printf("[%d] %s\n", _strlen(h->str), h->str);
-		else
-			printf("[0] (nil)\n");
-	h = h->next;
-	count++;
+		printf("[%d] %s\n", _strlen(h->str), h->str) ? h->str : "(nil)");
+		h = h->next;
+		count++;
 	}
 	return (count);
 }
