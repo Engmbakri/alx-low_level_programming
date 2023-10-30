b741386f4c841c736b7b4c545f56196b5e360ffb
Emgmbakri <mrm507415@gmail.com>
2023-10-30 08:50:46 -0700

    ll

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	for (n = 0; text_content[n]; n++)
 		;
 
-	rwr = write(fd, text_content, n);
+	rwr = write(ptr, text_content, n);
 
 	if (rwr == -1)
 		return (-1);
